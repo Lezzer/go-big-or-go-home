ff62af1d2f502fc67acb9188c466806a18cf92ce
Liam Reilly <liam.reilly@zuto.com>
2018-04-06 15:02:05 +0100

    Can now specify more than one group in the pipeline filter

--- content.js
+++ content.js
@@ -1,17 +1,24 @@
 chrome.storage.sync.get("options", function(settings) {
-    
     const pipelineSelector = document.getElementById("pipelines_selector_pipelines");
     pipelineSelector.style.backgroundColor = settings.options.backgroundColour;
     
     const pipelineGroups = pipelineSelector.getElementsByClassName("selector_group");
-   
-    const pipelineFilter = settings.options.pipelinesFilter;
+    const pipelineFilterArray = settings.options.pipelinesFilter;
     
     for(var i = 0; i < pipelineGroups.length; i++) {
         var pipelineGroup = pipelineGroups[i];     
-
-        if (pipelineGroup.id.toLowerCase().indexOf(pipelineFilter) === -1){
+        if (!isInPipelineFilterArray(pipelineGroup.id.toLowerCase(), pipelineFilterArray)){
             pipelineGroup.style.display = 'none';
         }
     }
 });
+
+function isInPipelineFilterArray(id, pipelineFilterArray) {
+    for(var i = 0; i < pipelineFilterArray.length; i++) {
+        var filter = pipelineFilterArray[i];
+        if (id.toLowerCase().indexOf(filter) !== -1) {
+            return true;
+        }
+    }
+    return false;
+}
