4650b4234c26613df319105e583cf165202f41ee
Liam Reilly <liam.reilly@zuto.com>
2018-04-06 15:51:24 +0100

    Now runs on 5 second interval so pipelines don't reset themselves.

--- content.js
+++ content.js
@@ -1,17 +1,21 @@
-chrome.storage.sync.get("options", function(settings) {
-    const pipelineSelector = document.getElementById("pipelines_selector_pipelines");
-    pipelineSelector.style.backgroundColor = settings.options.backgroundColour;
-    
-    const pipelineGroups = pipelineSelector.getElementsByClassName("selector_group");
-    const pipelineFilterArray = settings.options.pipelinesFilter;
-    
-    for(var i = 0; i < pipelineGroups.length; i++) {
-        var pipelineGroup = pipelineGroups[i];     
-        if (!isInPipelineFilterArray(pipelineGroup.id.toLowerCase(), pipelineFilterArray)){
-            pipelineGroup.style.display = 'none';
+function formatPipelinesDropDown() {
+    chrome.storage.sync.get("options", function(settings) {
+        const pipelineSelector = document.getElementById("pipelines_selector_pipelines");
+        pipelineSelector.style.backgroundColor = settings.options.backgroundColour;
+
+        const pipelineGroups = pipelineSelector.getElementsByClassName("selector_group");
+        const pipelineFilterArray = settings.options.pipelinesFilter;
+
+        for(var i = 0; i < pipelineGroups.length; i++) {
+            var pipelineGroup = pipelineGroups[i];
+            if (!isInPipelineFilterArray(pipelineGroup.id.toLowerCase(), pipelineFilterArray)){
+                pipelineGroup.style.display = 'none';
+            }
         }
-    }
-});
+    });
+
+}
+
 
 function isInPipelineFilterArray(id, pipelineFilterArray) {
     for(var i = 0; i < pipelineFilterArray.length; i++) {
@@ -22,3 +26,5 @@ function isInPipelineFilterArray(id, pipelineFilterArray) {
     }
     return false;
 }
+
+setInterval(formatPipelinesDropDown, 5000);
