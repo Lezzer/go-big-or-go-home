1430124769f3d85edb828e1710863a1fe949756c
Liam Reilly <liam.reilly.1@gmail.com>
2018-04-08 11:14:59 +0100

    Pipeline groups that are hidden from view in the drop down are now automatically removed from the page also.

--- content.js
+++ content.js
@@ -1,31 +1,47 @@
-function formatPipelinesDropDown() {
+function syncOptionsToPage() {
     chrome.storage.sync.get("options", function(settings) {
+
+        var pipelinesFilter = settings.options.pipelinesFilter;
+        var pipelinesFilterArray = pipelinesFilter.split(";");
+        
+        formatPipelinesDropDown(settings, pipelinesFilterArray);
+        hidePipelineGroupsOnPage(pipelinesFilterArray);
+    });
+}
+
+function formatPipelinesDropDown(settings, pipelinesFilterArray) {
         const pipelineSelector = document.getElementById("pipelines_selector_pipelines");
         pipelineSelector.style.backgroundColor = settings.options.backgroundColour;
 
         const pipelineGroups = pipelineSelector.getElementsByClassName("selector_group");
-        
-        const pipelinesFilter = settings.options.pipelinesFilter;
-        const pipelinesFilterArray = pipelinesFilter.split(";");
-        
         for(var i = 0; i < pipelineGroups.length; i++) {
             var pipelineGroup = pipelineGroups[i];
             if (!isInPipelineFilterArray(pipelineGroup.id.toLowerCase(), pipelinesFilterArray)){
                 pipelineGroup.style.display = 'none';
             }
         }
-    });
+}
+
+function hidePipelineGroupsOnPage(pipelinesFilterArray) {
+    const pagePipelineGroups = document.getElementById("pipeline_groups_container");
+    for(var i = 0; i < pagePipelineGroups.childNodes.length; i++) {
+        var pipelineGroup = pagePipelineGroups.children[i];
+        if (!isInPipelineFilterArray(pipelineGroup.id, pipelinesFilterArray)){
+            pipelineGroup.style.display = 'none';
+        }
+    }
+
 }
 
 function isInPipelineFilterArray(id, pipelineFilterArray) {
     for(var i = 0; i < pipelineFilterArray.length; i++) {
         var filter = pipelineFilterArray[i];
-        if (id.toLowerCase().indexOf(filter) !== -1) {
+        if (id.toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
             return true;
         }
     }
     return false;
 }
 
-formatPipelinesDropDown();
-setInterval(formatPipelinesDropDown, 5000);
+syncOptionsToPage();
+setInterval(syncOptionsToPage, 5000);
