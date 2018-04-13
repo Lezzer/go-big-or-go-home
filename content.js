e99175d85fd48d26d1ca32d262db92b94b7e8002
Liam Reilly <liam.reilly@zuto.com>
2018-04-13 15:39:21 +0100

    Few tweaks for performance. No longer runs on timer interval.

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-function syncOptionsToPage(filterPage) {
+function syncOptionsToPage() {
     chrome.storage.sync.get("options", function(settings) {
 
         var pipelinesFilter = settings.options.pipelinesFilter;
@@ -6,33 +6,35 @@ function syncOptionsToPage(filterPage) {
         
         formatPipelinesDropDown(settings, pipelinesFilterArray);
         
-        if (filterPage)
+        if (settings.options.filterPage === true)
             hidePipelineGroupsOnPage(pipelinesFilterArray);
     });
 }
 
 function formatPipelinesDropDown(settings, pipelinesFilterArray) {
-        const pipelineSelector = document.getElementById("pipelines_selector_pipelines");
-        pipelineSelector.style.backgroundColor = settings.options.backgroundColour;
+    const pipelineSelector = document.getElementById("pipelines_selector_pipelines");
+    pipelineSelector.style.backgroundColor = settings.options.backgroundColour;
 
-        const pipelineGroups = pipelineSelector.getElementsByClassName("selector_group");
-        for(var i = 0; i < pipelineGroups.length; i++) {
-            var pipelineGroup = pipelineGroups[i];
-            if (!isInPipelineFilterArray(pipelineGroup.id.toLowerCase(), pipelinesFilterArray)){
-                pipelineGroup.style.display = 'none';
-            }
+    const pipelineGroups = pipelineSelector.getElementsByClassName("selector_group");
+    for(var i = 0; i < pipelineGroups.length; i++) {
+        var pipelineGroup = pipelineGroups[i];
+        if (!isInPipelineFilterArray(pipelineGroup.id.toLowerCase(), pipelinesFilterArray)){
+            pipelineGroup.style.display = 'none';
         }
+    }
 }
 
 function hidePipelineGroupsOnPage(pipelinesFilterArray) {
     const pagePipelineGroups = document.getElementById("pipeline_groups_container");
     for(var i = 0; i < pagePipelineGroups.childNodes.length; i++) {
         var pipelineGroup = pagePipelineGroups.children[i];
-        if (!isInPipelineFilterArray(pipelineGroup.id, pipelinesFilterArray)){
-            pipelineGroup.style.display = 'none';
+        if (pipelineGroup !== undefined)
+        {
+            if (!isInPipelineFilterArray(pipelineGroup.id, pipelinesFilterArray)){
+                pipelineGroup.style.display = 'none';
+            } 
         }
     }
-
 }
 
 function isInPipelineFilterArray(id, pipelineFilterArray) {
@@ -45,5 +47,15 @@ function isInPipelineFilterArray(id, pipelineFilterArray) {
     return false;
 }
 
-syncOptionsToPage(true);
-setInterval(syncOptionsToPage, 5000);
+function buttonClicked() {
+    syncOptionsToPage();
+}
+
+function hookIntoPersonalizeButtonClick () {
+    const button = document.getElementById('show_pipelines_selector');
+    button.removeEventListener('click', buttonClicked);
+    button.addEventListener('click', buttonClicked);
+}
+
+syncOptionsToPage();
+hookIntoPersonalizeButtonClick();
